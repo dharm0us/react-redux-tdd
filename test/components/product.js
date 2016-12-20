95ea59a07f04f0b4d20f6ea708fa07b8c8b48e98
dharam@epicenterlabs.com <dharam@epicenterlabs.com>
2016-12-21 00:02:20 +0530

    pass props and test value

--- test/components/product.js
+++ test/components/product.js
@@ -1,11 +1,16 @@
+import React from 'react'
 import {expect} from 'chai'
 import {shallow} from 'enzyme'
-import React from 'react'
 import Product from '../../app/components/Product.jsx'
 
 describe('<Product />', () => {
 		it('should contain one div tag', () =>{
 				let wrapper = shallow(<Product />)
 				expect( wrapper.find('div')).to.have.length(1)
+				})
+		it('should render a product name', () =>{
+				let testName = 'lorem ip sum'
+				let wrapper = shallow(<Product name={testName}/>)
+				expect( wrapper.find('p').text()).to.eql(testName)
 				})
 		})
