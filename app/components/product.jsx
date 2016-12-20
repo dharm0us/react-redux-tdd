d4f21252086449e46f9fa0a552b81ec10d816daa
dharam@epicenterlabs.com <dharam@epicenterlabs.com>
2016-12-21 00:55:15 +0530

    lifecycle methods

--- app/components/product.jsx
+++ app/components/product.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
-let Product = ({id,name,cost, handleClick, label}) => (
+/*let Product = ({id,name,cost, handleClick, label}) => (
 		<div>
+		<p>{name}</p>
 		{name} {cost} <button onClick={() => handleClick(id)}> {label} </button>
 		</div>
 		)
-export default Product
+*/
+export default class Product extends React.Component {
+	componentDidMount() {
+		console.log('component did mount')
+	}
+	render() {
+		let {id,name, cost, handleClick, label} = this.props
+	        return(
+				<div>
+				<p>{name}</p>
+		{name} {cost} <button onClick={() => handleClick(id)}> {label} </button>
+				</div>
+		)
+	}
+}
--- test/components/product.js
+++ test/components/product.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import {expect} from 'chai'
-import {shallow} from 'enzyme'
+import sinon from 'sinon'
+import {shallow, mount} from 'enzyme'
 import Product from '../../app/components/Product.jsx'
 
 describe('<Product />', () => {
+		it('will call componentDidMount', () =>{
+				sinon.spy(Product.prototype, 'componentDidMount')
+				let wrapper = mount(<Product />)
+			expect(Product.prototype.componentDidMount).to.have.property('callCount',1)
+			Product.prototype.componentDidMount.restore()
+				})
 		it('should contain one div tag', () =>{
 				let wrapper = shallow(<Product />)
 				expect( wrapper.find('div')).to.have.length(1)
